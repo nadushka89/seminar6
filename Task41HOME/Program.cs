abca5cd49ecdfa7ca5ab89a369a7b4a15f72b5dd
Надюшка Рыбка <kitti-46@ya.ru>
2022-10-20 00:41:29 +0300

    added Task41

---
+++ Task41HOME/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры 
+M чисел. Посчитайте, сколько чисел больше 
+0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int[] InitArray(int length)
+{
+    int[] myArray = new int[length];
+    //int mass = int.Parse(Console.ReadLine());
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        myArray[i] = int.Parse(Console.ReadLine());
+    }
+    return myArray;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int GetPositiv(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((array[i] > 0))
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+Console.WriteLine("Введите размерность массива:");
+int length = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите массив ");
+int[] arr = InitArray(length);
+Console.WriteLine();
+PrintArray(arr);
+int count = GetPositiv(arr);
+Console.WriteLine($"Положительных чисел в массиве {count}");
